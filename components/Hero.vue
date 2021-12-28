40035dff48e2d2555a389a1f9ec319047df887db
Daniil Vysotskiy <dvysotskiy@it-one.ru>
2021-12-28 13:11:25 +0300

    fix: delete tech-status

--- components/Hero.vue
+++ components/Hero.vue
@@ -5,10 +5,6 @@
         <span class="font-normal dark:text-gray-300"> Hey, I am </span>{{ $config.devName}}</h2>
       <h3 class="font-bold text-primary dark:text-primary text-2xl md:text-3xl lg:text-4xl">a {{ $config.devRole}}</h3>
       <p class="text-lg lg:text-xl font-semibold">{{ $config.devDescription}}</p>
-
-      <div class="mt-5">
-        <TechStack/>
-      </div>
     </div>
   </div>
 </template>
